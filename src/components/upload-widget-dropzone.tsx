31a0a4524809b5d31c5180dfcc3ab0d27c6c2cba
Carlos Oliveira <carlos.burndev@gmail.com>
2026-07-02 20:18:06 -0300

    feat(components): adds components animations

--- src/components/upload-widget-dropzone.tsx
+++ src/components/upload-widget-dropzone.tsx
@@ -1,4 +1,6 @@
 import { useDropzone } from "react-dropzone";
+import { motion } from "motion/react";
+
 import CircularProgressBar from "./ui/circular-progress-bar";
 
 
@@ -18,7 +20,14 @@ export function UploadWidgetDropzone() {
   const uploadGlobalPercentage = 66;
 
   return (
-    <div className="px-3 flex flex-col gap-3">
+      <motion.div
+      className="px-3 flex flex-col gap-3"
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+      }}
+      transition={{ duration: 0.2 }}
+    >
       <div
         data-active={isDragActive}
         className="cursor-pointer text-zinc-400 bg-black/20 p-5 rounded-lg border border-zinc-700 border-dashed h-32 flex flex-col items-center justify-center gap-1 hover:border-zinc-600 transition-colors data-[active=true]:bg-indigo-500/10 data-[active=true]:border-indigo-500"
@@ -46,6 +55,6 @@ export function UploadWidgetDropzone() {
       <span className="text-xxs text-zinc-400">
         Only PNG and JPG files are supported.
       </span>
-    </div>
+    </motion.div>
   );
 }
