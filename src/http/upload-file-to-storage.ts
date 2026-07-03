49993396510659c68c1646a308f8cab8a348dabd
Carlos Oliveira <carlos.burndev@gmail.com>
2026-07-02 21:23:34 -0300

    feat: adds uploads cancel handler

--- src/http/upload-file-to-storage.ts
+++ src/http/upload-file-to-storage.ts
@@ -4,7 +4,14 @@ interface UploadFileToStorageParams {
   file: File
 }
 
-export async function uploadFileToStorage({ file }: UploadFileToStorageParams) {
+interface uploadFileToStorageOpts {
+  signal?: AbortSignal
+}
+
+export async function uploadFileToStorage(
+  { file }: UploadFileToStorageParams,
+  opts?: uploadFileToStorageOpts
+) {
   const data = new FormData()
 
   data.append("file", file)
@@ -13,6 +20,7 @@ export async function uploadFileToStorage({ file }: UploadFileToStorageParams) {
     headers: {
       "Content-Type": "multipart/form-data",
     },
+    signal: opts?.signal,
   })
 
   return { url: response.data.url }
