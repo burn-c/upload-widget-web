82da50cf15e709505a0e076fb0da9b651611d97b
Carlos Oliveira <carlos.burndev@gmail.com>
2026-07-02 22:49:07 -0300

    fix: compress resize

--- src/store/uploads.ts
+++ src/store/uploads.ts
@@ -13,7 +13,9 @@ export type Upload = {
   abortController: AbortController
   status: "progress" | "success" | "error" | "canceled"
   originalSizeInBytes: number
+  compressedSizeInBytes?: number
   uploadSizeInBytes: number
+  remoteUrl?: string
 }
 
 type UploadState = {
@@ -51,12 +53,14 @@ export const useUploads = create<UploadState, [["zustand/immer", never]]>(
       try {
         const compressedFile = await compressImage({
           file: upload.file,
-          maxWidth: 200,
-          maxHeight: 200,
-          quality: 0.5,
+          maxWidth: 1000,
+          maxHeight: 1000,
+          quality: 0.8,
         })
 
-        await uploadFileToStorage(
+        updateUpload(uploadId, { compressedSizeInBytes: compressedFile.size })
+
+        const { url } = await uploadFileToStorage(
           {
             file: compressedFile,
             onProgress(sizeInBytes) {
@@ -70,6 +74,7 @@ export const useUploads = create<UploadState, [["zustand/immer", never]]>(
 
         updateUpload(uploadId, {
           status: "success",
+          remoteUrl: url,
         })
       } catch (err) {
         if (err instanceof CanceledError) {
@@ -146,8 +151,11 @@ export const usePendingUploads = () => {
 
       const { total, uploaded } = Array.from(store.uploads.values()).reduce(
         (acc, upload) => {
-          acc.total += upload.originalSizeInBytes
-          acc.uploaded += upload.uploadSizeInBytes
+          if (upload.compressedSizeInBytes) {
+            acc.uploaded += upload.uploadSizeInBytes
+          }
+
+          acc.total += upload.compressedSizeInBytes || upload.originalSizeInBytes
 
           return acc
         },
