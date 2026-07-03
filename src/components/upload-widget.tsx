ffe7aab8b30c6bfffa8648a31a875a893f6e1ded
Carlos Oliveira <carlos.burndev@gmail.com>
2026-07-02 22:31:18 -0300

    feat: adds upload amount progress

--- src/components/upload-widget.tsx
+++ src/components/upload-widget.tsx
@@ -1,3 +1,4 @@
+import { usePendingUploads } from "../store/uploads"
 import { UploadWidgetDropzone } from "./upload-widget-dropzone"
 import { UploadWidgetHeader } from "./upload-widget-header"
 import { UploadWidgetMinimizedButton } from "./upload-widget-minimized-button"
@@ -6,13 +7,13 @@ import * as Collapsible from "@radix-ui/react-collapsible"
 import { motion, useCycle } from "motion/react"
 
 export function UploadWidget() {
+  const { isThereAnyPendingUploads } = usePendingUploads()
   const [isWidgetOpen, toggleWidgetOpen] = useCycle(false, true)
-  const isThereAnyPendindUploads = true
 
   return (
     <Collapsible.Root onOpenChange={() => toggleWidgetOpen()} asChild>
       <motion.div
-        data-progress={isThereAnyPendindUploads}
+        data-progress={isThereAnyPendingUploads}
         className="bg-zinc-900 overflow-hidden w-[360px] rounded-xl data-[state=open]:shadow-shape border border-transparent animate-border data-[state=closed]:rounded-3xl data-[state=closed]:data-[progress=false]:shadow-shape  data-[state=closed]:data-[progress=true]:[background:linear-gradient(45deg,#09090B,theme(colors.zinc.900)_50%,#09090B)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.zinc.700/.48)_80%,_theme(colors.indigo.500)_86%,_theme(colors.indigo.300)_90%,_theme(colors.indigo.500)_94%,_theme(colors.zinc.600/.48))_border-box]"
         animate={isWidgetOpen ? "open" : "closed"}
         variants={{
--- src/store/uploads.ts
+++ src/store/uploads.ts
@@ -4,6 +4,7 @@ import { create } from "zustand"
 
 import { uploadFileToStorage } from "../http/upload-file-to-storage"
 import { immer } from "zustand/middleware/immer"
+import { useShallow } from "zustand/shallow"
 
 export type Upload = {
   name: string
@@ -24,6 +25,21 @@ enableMapSet()
 
 export const useUploads = create<UploadState, [["zustand/immer", never]]>(
   immer((set, get) => {
+    function updateUpload(uploadId: string, data: Partial<Upload>) {
+      const upload = get().uploads.get(uploadId)
+
+      if (!upload) {
+        return
+      }
+
+      set(state => {
+        state.uploads.set(uploadId, {
+          ...upload,
+          ...data,
+        })
+      })
+    }
+
     async function processUpload(uploadId: string) {
       const upload = get().uploads.get(uploadId)
 
@@ -36,40 +52,28 @@ export const useUploads = create<UploadState, [["zustand/immer", never]]>(
           {
             file: upload.file,
             onProgress(sizeInBytes) {
-              set(state => {
-                state.uploads.set(uploadId, {
-                  ...upload,
-                  uploadSizeInBytes: sizeInBytes,
-                })
+              updateUpload(uploadId, {
+                uploadSizeInBytes: sizeInBytes,
               })
             },
           },
           { signal: upload.abortController.signal }
         )
 
-        set(state => {
-          state.uploads.set(uploadId, {
-            ...upload,
-            status: "success",
-          })
+        updateUpload(uploadId, {
+          status: "success",
         })
       } catch (err) {
         if (err instanceof CanceledError) {
-          set(state => {
-            state.uploads.set(uploadId, {
-              ...upload,
-              status: "canceled",
-            })
+          updateUpload(uploadId, {
+            status: "canceled",
           })
 
           return
         }
 
-        set(state => {
-          state.uploads.set(uploadId, {
-            ...upload,
-            status: "error",
-          })
+        updateUpload(uploadId, {
+          status: "error",
         })
       }
     }
@@ -120,3 +124,31 @@ export const useUploads = create<UploadState, [["zustand/immer", never]]>(
     }
   })
 )
+
+export const usePendingUploads = () => {
+  return useUploads(
+    useShallow(store => {
+      const isThereAnyPendingUploads = Array.from(store.uploads.values()).some(
+        upload => upload.status === "progress"
+      )
+
+      if (!isThereAnyPendingUploads) {
+        return { isThereAnyPendingUploads, globalPercentage: 100 }
+      }
+
+      const { total, uploaded } = Array.from(store.uploads.values()).reduce(
+        (acc, upload) => {
+          acc.total += upload.originalSizeInBytes
+          acc.uploaded += upload.uploadSizeInBytes
+
+          return acc
+        },
+        { total: 0, uploaded: 0 }
+      )
+
+      const globalPercentage = Math.min(Math.round((uploaded * 100) / total), 100)
+
+      return { isThereAnyPendingUploads, globalPercentage }
+    })
+  )
+}
