8b703f23e90499cbc586de5fde3d797ac5551d00
Carlos Oliveira <carlos.burndev@gmail.com>
2026-07-02 21:01:45 -0300

    perform: adds immer integration with Zustand

--- src/store/uploads.ts
+++ src/store/uploads.ts
@@ -1,4 +1,7 @@
+import { enableMapSet } from "immer"
 import { create } from "zustand"
+
+import { immer } from "zustand/middleware/immer"
 
 export type Upload = {
   name: string
@@ -10,26 +13,28 @@ type UploadState = {
   addUploads: (files: File[]) => void
 }
 
-export const useUploads = create<UploadState>((set, get) => {
-  function addUploads(files: File[]) {
-    for (const file of files) {
-      const uploadId = crypto.randomUUID()
+enableMapSet()
 
-      const upload: Upload = {
-        name: file.name,
-        file,
-      }
+export const useUploads = create<UploadState, [["zustand/immer", never]]>(
+  immer(set => {
+    function addUploads(files: File[]) {
+      for (const file of files) {
+        const uploadId = crypto.randomUUID()
 
-      set(state => {
-        return {
-          uploads: state.uploads.set(uploadId, upload),
+        const upload: Upload = {
+          name: file.name,
+          file,
         }
-      })
-    }
-  }
 
-  return {
-    uploads: new Map(),
-    addUploads,
-  }
-})
+        set(state => {
+          state.uploads.set(uploadId, upload)
+        })
+      }
+    }
+
+    return {
+      uploads: new Map(),
+      addUploads,
+    }
+  })
+)
