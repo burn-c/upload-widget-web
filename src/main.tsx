e9877b0094691fd7aa6e9414b3224ff19df124b3
Carlos Oliveira <carlos.burndev@gmail.com>
2026-07-16 00:11:35 -0300

    ci: upgrade node v24

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
-import "./index.css"
-
 import { StrictMode } from "react"
 
 import { App } from "./app"
 import { createRoot } from "react-dom/client"
+import "./index.css"
 
 // biome-ignore lint/style/noNonNullAssertion: False positive
 createRoot(document.getElementById("root")!).render(
