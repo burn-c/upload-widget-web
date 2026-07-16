d110634897b595f706acf601698a113acf06bf76
Carlos Oliveira <carlos.burndev@gmail.com>
2026-07-15 23:51:06 -0300

    chore: lint

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { StrictMode } from "react"
 import { App } from "./app"
 import { createRoot } from "react-dom/client"
 
+// biome-ignore lint/style/noNonNullAssertion: False positive
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
